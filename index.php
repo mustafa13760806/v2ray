33f34ea4c857a694751f5402690e065b413c7179
mustafa13760806 <75061017+mustafa13760806@users.noreply.github.com>
2023-12-29 19:23:56 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$url1 = 'https://raw.githubusercontent.com/mustafa13760806/v2ray/main/Matin'; // Replace with the URL you want to retrieve text from
+$file1 = 'https://raw.githubusercontent.com/your_username/your_repository/file1.txt'; // Replace with your GitHub file URL
+
+// Get the text from URL1
+$text = file_get_contents($url1);
+
+// Save the text in file1.txt on GitHub
+file_put_contents($file1, $text);
+
+echo "Text saved successfully.";

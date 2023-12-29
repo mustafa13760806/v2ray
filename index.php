6d76127a40e8fcf50ee2e56da297133173c5d114
mustafa13760806 <75061017+mustafa13760806@users.noreply.github.com>
2023-12-29 20:14:58 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,101 @@
-<?php
-// Function to retrieve text from the given URL
-function getTextFromURL($url) {
-  $text = file_get_contents($url);
-  return $text;
-}
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+<body>
+  <script>
+    // Function to fetch text from a given URL
+    function fetchText(url) {
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          url: url,
+          dataType: 'text',
+          success: resolve,
+          error: reject
+        });
+      });
+    }
 
-// URLs to fetch text from
-$url1 = "https://raw.githubusercontent.com/mustafa13760806/v2ray/main/Matin";
-$url2 = "https://raw.githubusercontent.com/mustafa13760806/v2ray/main/Matin";
-$url3 = "https://raw.githubusercontent.com/mustafa13760806/v2ray/main/Matin";
+    // GitHub API endpoint to create or update files
+    const apiUrl = "https://api.github.com/repos/your-username/your-repo/contents/";
 
-// Fetch text from each URL
-$text1 = getTextFromURL($url1);
-$text2 = getTextFromURL($url2);
-$text3 = getTextFromURL($url3);
+    // URLs to fetch texts from
+    const url1 = "https://raw.githubusercontent.com/mustafa13760806/v2ray/main/Matin";
 
-// Save text to files
-file_put_contents('file1.txt', $text1);
-file_put_contents('file2.txt', $text2);
-file_put_contents('file3.txt', $text3);
+    // File paths on GitHub
+    const filePath1 = "file1.txt";
+    const filePath2 = "file2.txt";
+    const filePath3 = "file3.txt";
 
-// GitHub credentials and repository details
-$githubUsername = 'mustafa13760806';
-$githubPassword = 'n58nneawyraj7kd';
-$repositoryOwner = 'mustafa13760806';
-$repositoryName = 'v2ray';
+    // Fetch text from URL1 and save it in file1.txt
+    fetchText(url1)
+      .then(text => {
+        const file1Content = btoa(text); // Encode text content in base64
 
-// Commit and push changes to GitHub
-$commitMessage = 'Updated files';
-$baseGitURL = "https://{$githubUsername}:{$githubPassword}@github.com/{$repositoryOwner}/{$repositoryName}.git";
+        // Create or update file1.txt on GitHub
+        $.ajax({
+          url: apiUrl + filePath1,
+          type: 'PUT',
+          dataType: 'json',
+          data: JSON.stringify({
+            message: 'Updated file1.txt',
+            content: file1Content
+          }),
+          beforeSend: function(xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer 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");
+          },
+          success: function(result) {
+            console.log("file1.txt saved successfully");
+          },
+          error: function(error) {
+            console.error("Error saving file1.txt:", error);
+          }
+        });
 
-// Function to execute shell commands
-function executeShellCommand($command) {
-  $output = shell_exec($command);
-  echo $output;
-}
+        // Save the same content in file2.txt and file3.txt
+        // Update the 'filePath2' and 'filePath3' values accordingly
+        $.ajax({
+          url: apiUrl + filePath2,
+          type: 'PUT',
+          dataType: 'json',
+          data: JSON.stringify({
+            message: 'Updated file2.txt',
+            content: file1Content
+          }),
+          beforeSend: function(xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer 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");
+          },
+          success: function(result) {
+            console.log("file2.txt saved successfully");
+          },
+          error: function(error) {
+            console.error("Error saving file2.txt:", error);
+          }
+        });
 
-// Change directory to the local repository
-chdir('/path/to/your/local/repo');
-
-// Commit and push changes to GitHub
-executeShellCommand("git add file1.txt file2.txt file3.txt");
-executeShellCommand("git commit -m '{$commitMessage}'");
-executeShellCommand("git pull {$baseGitURL}");
-executeShellCommand("git push {$baseGitURL}");
-
-echo "Files successfully updated on GitHub
-";
-?>
+        $.ajax({
+          url: apiUrl + filePath3,
+          type: 'PUT',
+          dataType: 'json',
+          data: JSON.stringify({
+            message: 'Updated file3.txt',
+            content: file1Content
+          }),
+          beforeSend: function(xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer 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");
+          },
+          success: function(result) {
+            console.log("file3.txt saved successfully");
+          },
+          error: function(error) {
+            console.error("Error saving file3.txt:", error);
+          }
+        });
+      })
+      .catch(error => {
+        console.error("Error fetching text from URL1:", error);
+      });
+  </script>
+</body>
+</html>

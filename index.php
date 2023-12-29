4d2c2f9948c1537eea41564781d002d4db6bc895
mustafa13760806 <75061017+mustafa13760806@users.noreply.github.com>
2023-12-29 19:53:52 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,50 @@
 <?php
+// Function to retrieve text from the given URL
+function getTextFromURL($url) {
+  $text = file_get_contents($url);
+  return $text;
+}
 
-$url1 = 'https://raw.githubusercontent.com/mustafa13760806/v2ray/main/Matin'; // Replace with the URL you want to retrieve text from
-$file1 = 'https://raw.githubusercontent.com/mustafa13760806/v2ray/main/File1.txt'; // Replace with your GitHub file URL
+// URLs to fetch text from
+$url1 = "https://raw.githubusercontent.com/mustafa13760806/v2ray/main/Matin";
+$url2 = "https://raw.githubusercontent.com/mustafa13760806/v2ray/main/Matin";
+$url3 = "https://raw.githubusercontent.com/mustafa13760806/v2ray/main/Matin";
 
-// Get the text from URL1
-$text = file_get_contents($url1);
+// Fetch text from each URL
+$text1 = getTextFromURL($url1);
+$text2 = getTextFromURL($url2);
+$text3 = getTextFromURL($url3);
 
-// Save the text in file1.txt on GitHub
-file_put_contents($file1, $text);
+// Save text to files
+file_put_contents('file1.txt', $text1);
+file_put_contents('file2.txt', $text2);
+file_put_contents('file3.txt', $text3);
 
-echo "Text saved successfully.";
+// GitHub credentials and repository details
+$githubUsername = 'mustafa13760806';
+$githubPassword = 'n58nneawyraj7kd';
+$repositoryOwner = 'mustafa13760806';
+$repositoryName = 'v2ray';
+
+// Commit and push changes to GitHub
+$commitMessage = 'Updated files';
+$baseGitURL = "https://{$githubUsername}:{$githubPassword}@github.com/{$repositoryOwner}/{$repositoryName}.git";
+
+// Function to execute shell commands
+function executeShellCommand($command) {
+  $output = shell_exec($command);
+  echo $output;
+}
+
+// Change directory to the local repository
+chdir('/path/to/your/local/repo');
+
+// Commit and push changes to GitHub
+executeShellCommand("git add file1.txt file2.txt file3.txt");
+executeShellCommand("git commit -m '{$commitMessage}'");
+executeShellCommand("git pull {$baseGitURL}");
+executeShellCommand("git push {$baseGitURL}");
+
+echo "Files successfully updated on GitHub
+";
 ?>

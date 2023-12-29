2aa39724e01545a8b19149e19f12653b9307c2e4
mustafa13760806 <75061017+mustafa13760806@users.noreply.github.com>
2023-12-29 19:47:17 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $url1 = 'https://raw.githubusercontent.com/mustafa13760806/v2ray/main/Matin'; // Replace with the URL you want to retrieve text from
-$file1 = 'https://tellso.ir/v2ray/File1.txt'; // Replace with your GitHub file URL
+$file1 = 'https://raw.githubusercontent.com/mustafa13760806/v2ray/main/File1.txt'; // Replace with your GitHub file URL
 
 // Get the text from URL1
 $text = file_get_contents($url1);
@@ -10,3 +10,4 @@ $text = file_get_contents($url1);
 file_put_contents($file1, $text);
 
 echo "Text saved successfully.";
+?>
